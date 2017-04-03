06a1304dc72b9bfb1b91a4f4ec346e660457549e
yjsltt <409898949@qq.com>
2017-04-03 22:36:39 +0800

    mistake

--- js/project2.js
+++ js/project2.js
@@ -18,29 +18,14 @@ function init() {
     // var axes=new THREE.AxisHelper(60);
     // scene.add(axes);
 
-    // var loader=new THREE.ImageLoader();
-    // loader.load(
-    // // resource URL
-    // 'textures/skyboxsun25degtest.png',
-    //     // Function when resource is loaded
-    //     function ( image ) {
-    //         // do something with it
-    //         // like drawing a part of it on a canvas
-    //         var canvas = document.createElement( 'canvas' );
-    //         var context = canvas.getContext( '2d' );
-    //         context.drawImage( image, 100, 100 );
-    //     },
-    //     // Function called when download progresses
-    //     function ( xhr ) {
-    //         console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-    //     },
-    //     // Function called when download errors
-    //     function ( xhr ) {
-    //         console.log( 'An error happened' );
-    //     }
-    // );
+
     // var bacGeo=new THREE.PlaneGeometry(window.innerWidth,window.innerHeight);
-    // var bacMater=new THREE.MeshMaterial()
+    // var texture=new THREE.TextureLoader().load("images/alex.png");
+    // var bacMater=new THREE.MeshLambertMaterial({map:texture,side:THREE.DoubleSide});
+    // var bac=new THREE.Mesh(bacGeo,bacMater);
+    // bac.receiveShadow=true;
+    // bac.position.set(-45,0,0);
+    // scene.add(bac);
     var planeGeometry = new THREE.PlaneGeometry(100, 20);
     var planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
     var plane = new THREE.Mesh(planeGeometry, planeMaterial);
