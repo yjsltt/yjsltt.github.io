02d304ab4e5efb51db09c8d7f38936b4b134ab31
yjsltt <409898949@qq.com>
2017-06-20 00:50:34 +0800

    final push v4?

--- js/FinalPro/Ray3.js
+++ js/FinalPro/Ray3.js
@@ -3,7 +3,7 @@
  * @requires Color
  */
 
-Ray3 = function(origin, direction) { this.origin = origin; this.direction = direction; }
+Ray3 = function(origin, direction) { this.origin = origin; this.direction = direction; };
 
 Ray3.prototype = {
     getPoint : function(t) { return this.origin.add(this.direction.multiply(t)); }
